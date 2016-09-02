7455cab5658c472bf9b4494f42b9917c090ec08b
GERBEAR <gerkeo.89@gmail.com>
2016-09-02 11:50:32 +0100

    Adding Ger Activity

---
+++ app/src/main/java/ie/iskill/collaborationexample/ger/GerActivity.java
@@ -0,0 +1,15 @@
+package ie.iskill.collaborationexample.ger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.iskill.collaborationexample.R;
+
+public class GerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ger);
+    }
+}
