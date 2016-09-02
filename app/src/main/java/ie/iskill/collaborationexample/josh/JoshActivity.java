4f4a8720223c3b8134a55e7dbee0f98de481ecb9
Dean Harrison <deanharrisonapps@gmail.com>
2016-09-02 12:03:55 +0100

    Added dummy activity for students to work on

---
+++ app/src/main/java/ie/iskill/collaborationexample/josh/JoshActivity.java
@@ -0,0 +1,15 @@
+package ie.iskill.collaborationexample.josh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.iskill.collaborationexample.R;
+
+public class JoshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_josh);
+    }
+}
