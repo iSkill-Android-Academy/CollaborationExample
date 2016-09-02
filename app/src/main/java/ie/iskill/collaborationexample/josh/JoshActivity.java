a6840d6dfff19822331fa57a1e0f7b2bf55deafe
joshcarroll93 <93.joshuacarroll@gmail.com>
2016-09-02 14:38:41 +0100

    josh about page

--- app/src/main/java/ie/iskill/collaborationexample/josh/JoshActivity.java
+++ app/src/main/java/ie/iskill/collaborationexample/josh/JoshActivity.java
@@ -1,7 +1,10 @@
 package ie.iskill.collaborationexample.josh;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ie.iskill.collaborationexample.R;
 
@@ -11,5 +14,17 @@ public class JoshActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_josh);
+    }
+    public void onDonateClick(View View){
+        goToUrl("https://www.iprt.ie/donation-form");
+
+    }
+    public void onIskillClick(View view){
+        goToUrl("http://www.iskill.ie/");
+    }
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
