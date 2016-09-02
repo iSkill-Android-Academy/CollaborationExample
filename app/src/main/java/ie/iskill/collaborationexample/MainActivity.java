701eec4aefe198a55a7ba96b109533ece5558ba6
Dean Harrison <deanharrisonapps@gmail.com>
2016-09-02 12:09:40 +0100

    Added handler to change activity

--- app/src/main/java/ie/iskill/collaborationexample/MainActivity.java
+++ app/src/main/java/ie/iskill/collaborationexample/MainActivity.java
@@ -1,7 +1,15 @@
 package ie.iskill.collaborationexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import ie.iskill.collaborationexample.christina.ChristinaActivity;
+import ie.iskill.collaborationexample.eric.EricActivity;
+import ie.iskill.collaborationexample.ger.GerActivity;
+import ie.iskill.collaborationexample.josh.JoshActivity;
+import ie.iskill.collaborationexample.tomas.TomasActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.christina_button : intent = new Intent(this, ChristinaActivity.class);break;
+            case R.id.eric_button : intent = new Intent(this, EricActivity.class);break;
+            case R.id.ger_button : intent = new Intent(this, GerActivity.class); break;
+            case R.id.josh_button : intent = new Intent(this, JoshActivity.class); break;
+            case R.id.tom_button : intent = new Intent(this, TomasActivity.class); break;
+        }
+        if(intent != null) {
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/ie/iskill/collaborationexample/christina/ChristinaActivity.java
@@ -0,0 +1,15 @@
+package ie.iskill.collaborationexample.christina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.iskill.collaborationexample.R;
+
+public class ChristinaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_christina);
+    }
+}
